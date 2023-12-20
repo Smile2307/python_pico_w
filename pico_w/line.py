ae97706e3996b6d388c6cc8572b80c68978761ae
smile Lin <smile.lin309@gmail.com>
2023-12-20 22:24:30 +0800

    Create line.py

---
+++ pico_w/line.py
@@ -0,0 +1,28 @@
+# Import Library
+from linenotify import LineNotify
+from network import WLAN,STA_IF
+
+# Network Setup
+#ssid = '<ssid>'
+#password = '<password>'
+#ssid = 'Robert_iPhone'
+#password = '0926656000'
+ssid = 'Smilewireless'
+password = '0939763309'
+wlan = WLAN(STA_IF)
+wlan.active(True)
+print('Connecting...')
+wlan.connect(ssid,password)
+while not wlan.isconnected():
+    pass
+print(wlan.ifconfig())
+
+# Set Line Token 
+#line = LineNotify('<token>')
+line = LineNotify('RRkyUxrZa8jjo0DPRchWdSqsjkOdyOUEY1Di5GSHqRO')
+# Notify text message 
+line.notify('Hello World!')
+# Notify sticker with message
+line.notifySticker(3,240,'Nice Sticker')
+# Notify image from URL with message
+line.notifyImageURL('https://static.wikia.nocookie.net/chainsaw-man/images/1/1b/Pochita.PNG','Pochita')
