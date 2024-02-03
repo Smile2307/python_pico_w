7aca9cc793a7596a6ca471f27b76557197fd209d
Smile2307 <101440722+Smile2307@users.noreply.github.com>
2024-02-03 06:26:52 +0000

    modify

--- GUI/main.py
+++ GUI/main.py
@@ -1,6 +1,19 @@
+
 import streamlit as st
 import pandas as pd
+import requests
 
-st.title("Pico_W 職能發展協會專案")
+st.title("Pico_W_職能發展協會專案")
 st.header("雞舍:red[溫度]和:blue[光線]狀態")
 st.divider()
+
+url='https://blynk.cloud/external/api/get?token=V4RM4y9Q0-JaqBbF4ut1sHoO1lef-6Z3&v1&v2&v0'
+
+response = requests.request("GET",url)
+if response.status_code == 200:
+    all_data = response.json()
+    st.info(f'led:{all_data["v0"]}')
+    st.warning(f'光線:{all_data["v1"]}')
+    st.warning(f'可變電阻:{all_data["v2"]}')
+else:
+    st.write("連線失敗,請等一下再試")
