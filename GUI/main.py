0341deb28ca7530f875bcb6c2e4fa9831c3c0025
Smile2307 <101440722+Smile2307@users.noreply.github.com>
2024-02-03 06:50:20 +0000

    modify

--- GUI/main.py
+++ GUI/main.py
@@ -1,7 +1,11 @@
 
+
 import streamlit as st
 import pandas as pd
 import requests
+from streamlit_autorefresh import st_autoresh
+
+st_autoresh(interval=5000)
 
 st.title("Pico_W_職能發展協會專案")
 st.header("雞舍:red[溫度]和:blue[光線]狀態")
@@ -13,7 +17,7 @@ response = requests.request("GET",url)
 if response.status_code == 200:
     all_data = response.json()
     st.info(f'led:{all_data["v0"]}')
-    st.warning(f'光線:{all_data["v1"]}')
+    st.info(f'光線:{all_data["v1"]}')
     st.warning(f'可變電阻:{all_data["v2"]}')
 else:
     st.write("連線失敗,請等一下再試")
